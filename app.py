c6f434509c9b94373b2386938c91b2b4866b0ef6
soaresilva <ciente@gmail.com>
2020-04-27 17:35:56 +0200

    added registration form

--- app.py
+++ app.py
@@ -1,47 +1,30 @@
 from flask import Flask, render_template, flash, redirect, url_for, session, logging, request
-from data import articles
-from wtforms import Form, StringField, TextAreaField, PasswordField, validators
-# from flask_mysqldb import MySQL
-# from passlib.hash import sha256_crypt
+from flask_mysqldb import MySQL
+from wtforms import Form, StringField, validators
 
 app = Flask(__name__)
 
-Articles = articles()
+# Config MySQL
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_PORT'] = '3306'
+app.config['MYSQL_UNIX_SOCKET'] = '/Applications/mampstack-7.3.13-0/mysql/tmp/mysql.sock'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'rootroot'
+app.config['MYSQL_DB'] = 'flask_event_reg'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+mysql = MySQL(app)
 
 
 @app.route("/")
-def welcome():
-    return render_template("welcome.html",
-                           message="Here's a message from the view.")
+def index():
+    return render_template("welcome.html")
 
 
-# About
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-
-# Articles
-@app.route('/articles')
-def articles():
-    return render_template('articles.html', articles=Articles)
-
-
-# Each article
-@app.route('/article/<string:id>/')
-def each_article(id):
-    return render_template('article.html', id=id)
-
-
+# Register form class
 class RegisterForm(Form):
     name = StringField('Name', [validators.Length(min=1, max=50)])
-    username = StringField('Username', [validators.Length(min=4, max=25)])
     email = StringField('Email', [validators.Length(min=6, max=50)])
-    password = PasswordField('Password', [
-        validators.DataRequired(),
-        validators.EqualTo('confirm', message='Passwords do not match')
-        ])
-    confirm = PasswordField('Confirm Password')
+    event = StringField('Event', [validators.Length(min=4, max=25)])
 
 
 # Register
@@ -49,9 +32,26 @@ class RegisterForm(Form):
 def register():
     form = RegisterForm(request.form)
     if request.method == 'POST' and form.validate():
-        return render_template('register.html')
+        name = form.name.data
+        email = form.email.data
+        event = form.event.data
+
+        # Cursor
+        cur = mysql.connection.cursor()
+
+        cur.execute("INSERT INTO users(name, email, event) VALUES(%s, %s, %s)", (name, email, event))
+
+        # Send to DB
+        mysql.connection.commit()
+
+        # Close connection
+        cur.close()
+
+        flash('Thanks for your registration.', 'success')
+
     return render_template('register.html', form=form)
 
 
 if __name__ == '__main__':
+    app.secret_key('secret123')
     app.run(debug=True)
