e648165b00bd00487ef4f94607448538874fb3bd
soaresilva <ciente@gmail.com>
2020-04-27 16:02:14 +0200

    first commit

---
+++ data.py
@@ -0,0 +1,25 @@
+def articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'Blablalabalablablablb',
+            'author': 'Diogo',
+            'create_date': '2020-04-27'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'Blablalabagsfhghagflablablablb',
+            'author': 'Someone',
+            'create_date': '2020-04-27'
+        },
+        {
+            'id': 3,
+            'title': 'Article Three',
+            'body': '00000',
+            'author': 'Someone else',
+            'create_date': '2020-04-27'
+        },
+    ]
+    return articles
